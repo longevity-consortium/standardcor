ec0aa0eff170772bd80ce80db47d1a9b4be93de8
maxbox63 <max.robinson@systemsbiology.org>
2024-08-08 12:17:17 -1000

    Updated documentation and vignette

--- R/standardizeFromModel.R
+++ R/standardizeFromModel.R
@@ -1,16 +1,16 @@
 #' Build a standardized correlation matrix using null models for each pair of component datasets
 #'
-#' Uses the provided list of null models (${\rm modelL}$) to standardize each type of
+#' Uses the provided list of null models (modelL) to standardize each type of
 #' correlations to the target common null model specified by the v.std parameter,
-#' $(r + 1)/2 ~ {\rm Beta}({\rm v.std}, {\rm v.std})$, where $r$ is the
+#' (r + 1)/2 ~ Beta(v.std, v.std), where r is the
 #' standardized Spearman correlation coefficient between any two analytes
 #' in the modeled list of 'Omics datasets. These analtyes are listed by
 #' dataset in the components of the list parameter analtyeL, and must
 #' be unique and contain all analytes provided in each raw correlation matrix
-#' in ${\rm modelL}$. The resulting standardized correlation matrix is built blockwise and is full symmetric.
+#' in modelL. The resulting standardized correlation matrix is built blockwise and is full symmetric.
 #'
-#' @param modelL A list-of-lists structure providing two inputs for each pair of 'Omics datasets $A$ and $B$: a matrix ${\rm modelL}\[\[A\]\]\[\[B\]\]\[\['cor'\]\]$ of raw Spearman correlation values, and shape parameters ${\rm modelL}\[\[A\]\]\[\[B\]\]\[\['shape'\]\] == {\rm c}\(v,w\)$ specifying a null model $(1+r_{raw})/2 ~ {\rm Beta}(v,w)$ for the raw correlations.
-#' @param analtyeL For each 'Omics dataset $A$, ${\rm analyteL}\[\[A\]\]$ lists the analytes provided by dataset $A$. These analyte identifiers are required to be unique across all datasets.
+#' @param modelL A list-of-lists structure providing two inputs for each pair of 'Omics datasets A and B: a matrix modelL[[A]][[B]][['cor']] of raw Spearman correlation values, and shape parameters modelL[[A]][[B]][['shape']] == c(v,w) specifying a null model (1+r_raw)/2 ~ Beta(v,w) for the raw correlations.
+#' @param analtyeL For each 'Omics dataset A, analyteL[[A]] lists the analytes provided by dataset A. These analyte identifiers are required to be unique across all datasets.
 #' @param v.std 
 #' @return A symmetric matrix containing standardized Spearman correlation coefficients for every pair of analytes across all datasets.
 #'
