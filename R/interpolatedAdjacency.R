9823e460e1b5759d235b9e07fa18089d39864e19
maxbox63 <max.robinson@systemsbiology.org>
2024-08-13 10:22:43 -1000

    Updated interpolatedAdjacency and documentation

--- R/interpolatedAdjacency.R
+++ R/interpolatedAdjacency.R
@@ -5,20 +5,25 @@
 #'
 #' @param r A numeric object containing (standardized) correlation coefficients
 #' @param adjTable A tabulated soft-threshold adjacency function, as produced by nullModelAdjacencyTable()
-#' @return A numeric object containing the adjacencies for a correlation network
+#' @return A numeric object containing adjacencies for a correlation network model
 #'
 #' @export
 interpolatedAdjacency <- function(r, adjTable) {
   n     <- dim(adjTable)[1]
   x.min <- adjTable$x[1]
-  lo <- min(max(floor((r-x.min) / (adjTable$x[n]-x.min)), 1), n-1)
+  lo <- sapply(r,
+    function(x, n, x.min, x.max) {
+      k <- floor(n*(x-x.min) / (x.max-x.min))
+      return(ifelse(k < 1, 1, ifelse(k < n-1, k, n-1)))
+    }, n-1, x.min, adjTable$x[n])
   a  <- adjTable$x[lo]
   b  <- adjTable$x[lo+1]
-  return (ifelse(a < x,
-          ifelse(x < b,
-                 (adjTable$y[lo]*(b-x) + adjTable$y[lo+1]*(x-a)) / (b-a),
+  return (ifelse(a < r,
+          ifelse(r < b,
+                 (adjTable$y[lo]*(b-r) + adjTable$y[lo+1]*(r-a)) / (b-a),
                   adjTable$y[lo+1]),
                   adjTable$y[lo]))
 }
+
 
 # end of interpolatedAdjacency.R
