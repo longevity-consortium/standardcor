d3da139d806cde1dbafe660f5b7d2cdc0a3a783a
Max Robinson <38920052+maxbox63@users.noreply.github.com>
2024-11-18 10:05:11 -1000

    Update euclidDistance.R
    This function does NOT compute the Euclidean distance, it computes Euclidean distance enhanced by a Beta distribution background model! This needs to be corrected.

--- R/euclidDistance.R
+++ R/euclidDistance.R
@@ -1,8 +1,6 @@
 #' Calculate Euclidean distance
 #'
-#' Calculates distance network from correlation coefficient matrix using a Euclidean function.
-#'
-#'
+#' Calculates the Euclidean distances equivalent to a matrix of correlation coefficients. The Pearson correlation coefficient between two vectors is the cosine of the angle formed between the two vectors when placed with their tales at the origin. The corresponding Euclidean distance between the two vectors is the length of the vector difference between two unit vectors in the same direction as the input vectors, a value that is 0 when the two vectors point in the same direction, 1 when they are perpendicular, and 2 when they point in opposite directions.
 #'
 #' @param mat A matrix of correlation coefficients
 #' @param v The first parameter of the beta distribution
