6dadbcb5c33584451cfb42693eeea9419af0a277
Max Robinson <38920052+maxbox63@users.noreply.github.com>
2024-11-19 09:55:59 -1000

    Update euclidDistance.R
    Eliminated incorrect euclidDistance() function, and replaced undocumented functions e2d() and ue2d() with a documented squaredEuclidean() function that implements both.

--- R/euclidDistance.R
+++ R/euclidDistance.R
@@ -1,21 +1,35 @@
-#' Calculate Euclidean distance
+#' Squared Euclidean Distances
 #'
-#' Calculates the Euclidean distances equivalent to a matrix of correlation coefficients. The Pearson correlation coefficient between two vectors is the cosine of the angle formed between the two vectors when placed with their tales at the origin. The corresponding Euclidean distance between the two vectors is the length of the vector difference between two unit vectors in the same direction as the input vectors, a value that is 0 when the two vectors point in the same direction, 1 when they are perpendicular, and 2 when they point in opposite directions.
-#'
-#' @param mat A matrix of correlation coefficients
-#' @param v The first parameter of the beta distribution
-#' @param w The second parameter of the beta distribution
-#' @param type The type of distance to calculate
-#' @return A matrix of distances
+#' Returns the signed or unsigned squared Euclidean distance equivalent to (a matrix of) correlation coefficients,
+#' scaled to the range 0 to 1.
+#' 
+#' The Pearson correlation coefficient of a paired sample { (x_i, y_i) } is defined geometrically
+#' in the 2D plane defined in n dimensions by the origin, the point x = <x_i>, and the point y = <y_i>.
+#' Using 1 = <1,0> to represent the unit vector in the x-direction in the plane, let the y-direction
+#' in the plane be represented by unit vector u. The correlation coefficient r = r(x,y) = r({(x_i, y_i)})
+#' is the cosine of the angle from 1 to u.
+#' 
+#' The corresponding Euclidean distance is the length of u - 1, but it turns out that the square of the
+#' Euclidean distance is simply 1 - r. Note that while a correlation coefficient may be positive or
+#' negative, both positive and negative correlation indicate that the two values in the pair are
+#' associated, in the sense that knowing one can help predict the other. We can therefore use the
+#' (signed) correlation coefficient or its absolute value, a measure of (unsigned) association, to
+#' convert correlation to distance. The range of (squared) signed correlation distances defined above
+#' is [0, 2], but this function rescales the signed version to have the same [0,1] range as the
+#' (squared) unsigned version.
+#' 
+#' @param r A matrix of correlation coefficients
+#' @param type The type of distance to calculate: 'unsigned' (Default) or 'signed'. The unsigned squared distance of independent samples is 1, and perfectly correlated and anticorrelated samples reach distance 0. For signed squared distances, perfectly anticorrelated samples have distance 1, uncorrelated samples have distance 1/2, and only perfectly correlated samples reach distance 0.
+#' @return A numeric (matrix, array, or scalar, depending on r) of distances
 #'
 #' @export
-euclidDistance <- function(mat, v=1, w=1, type="unsigned") {
+squaredEuclidean <- function(r, type="unsigned") {
   if (type == "unsigned") {
-    return((ue2d(r) + dbeta((1+abs(r))/2, v, v)) / (1 + dbeta(1/2, v, v)))
+    d <- 1 - abs(r)  # naturally 0 to 1
   } else {
-    return((e2d(r) + pbeta((1+r)/2, v, w, lower.tail = FALSE))/2)
+    d <- (1 - r) / 2 # naturally 0 to 2, scaled down to 0 to 1
   }
+  return(d)
 }
 
-e2d <- function(r) { return ((1 - r)/2) }
-ue2d <- function(r) { return(1-abs(r)) }
+# end of squaredEuclidean.R
